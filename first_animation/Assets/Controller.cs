3ee4af5f2aa41925588edc98e298aa35a162b11d
biology-student <tsuka0625yskz@gmail.com>
2021-03-16 09:59:44 +0900

    first commit
    first_animationフォルダをコミット

---
+++ first_animation/Assets/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Controller : MonoBehaviour
+{
+    public float _speed = 1f;
+
+    private Rigidbody _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_rigidbody.velocity.magnitude < _speed)
+        {
+
+            float value = Input.GetAxis("Vertical");
+            if (value != 0)
+                _rigidbody.AddForce(0, 0, value * Time.fixedDeltaTime * 1000f);
+        }
+    }
+}
