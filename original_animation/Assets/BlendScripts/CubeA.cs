39de5136deefd4b7118d46ce9ac6b05b940069df
biology-student <tsuka0625yskz@gmail.com>
2021-03-16 11:51:15 +0900

    Blend Constraint を使用して学習
    ２つのゲームオブジェクトの位置と回転を合体させるアニメーション

---
+++ original_animation/Assets/BlendScripts/CubeA.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeA : MonoBehaviour
+{
+
+    public Vector3 positionChange;
+    public float limit;
+
+    private int check;
+    private float minLimit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        check = 0;
+        minLimit = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (check == 0)
+        {
+            transform.localPosition += positionChange;
+        }
+        else
+        {
+            transform.localPosition -= positionChange;
+        }
+        if (transform.localPosition.x >= limit || transform.localPosition.y >= limit || transform.localPosition.z >= limit)
+        {
+            check = 1;
+        }
+        else if(transform.localPosition.x <= minLimit && transform.localPosition.y <= minLimit && transform.localPosition.z <= minLimit)
+        {
+            check = 0;
+        }
+    }
+}
