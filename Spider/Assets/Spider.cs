78ea200f2731d623787615617bce4b636b985eb7
biology-student <tsuka0625yskz@gmail.com>
2021-03-18 20:43:51 +0900

    Spider Step6 まで完了

--- Spider/Assets/Spider.cs
+++ Spider/Assets/Spider.cs
@@ -13,6 +13,8 @@ public class Spider : MonoBehaviour
     private GameObject targetDistance;
     private LineRenderer lineRenderer;
 
+    public float maxDistance = 1f;
+    private float distance;
 
     void Start()
     {
@@ -21,6 +23,7 @@ public class Spider : MonoBehaviour
 
         legGroundTarget = transform.Find("Model/BodyPivot/Body/LegTargetRaycast/LegGroundTarget").gameObject;
         targetDistance = transform.Find("Model/BodyPivot/UpLegPivot/LegPivot/FootPivot/TargetDistance").gameObject;
+
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.startWidth = 0.1f;
         lineRenderer.endWidth = 0.1f;
@@ -31,13 +34,22 @@ public class Spider : MonoBehaviour
     {
         leftFootTarget.position = lastLeftFootPos;
         lastLeftFootPos = leftFootTarget.position;
+        Vector3 legGroundTargetPos = legGroundTarget.transform.position;
+        Vector3 targetDistancePos = targetDistance.transform.position;
 
         var positions = new Vector3[]
         {
             legGroundTarget.transform.position,
             targetDistance.transform.position
         };
-
         lineRenderer.SetPositions(positions);
+
+
+        distance = (legGroundTargetPos - targetDistancePos).magnitude;
+        if (distance >= maxDistance)
+        {
+            leftFootTarget.position = legGroundTargetPos + new Vector3(0, 1f, 0);
+            lastLeftFootPos = leftFootTarget.position;
+        }
     }
 }
