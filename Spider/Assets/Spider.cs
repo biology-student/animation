7e6f320336ccbe81c8216eacfbe92e51d305295b
biology-student <tsuka0625yskz@gmail.com>
2021-03-18 17:48:30 +0900

    Spider Step5 まで完了
    LineRendererを用いただけで距離を算出していないので注意

--- Spider/Assets/Spider.cs
+++ Spider/Assets/Spider.cs
@@ -8,12 +8,22 @@ public class Spider : MonoBehaviour
     public Transform leftFootTarget;
     private Vector3 initLeftFootPos;
     private Vector3 lastLeftFootPos;
-    
-    
+
+    private GameObject legGroundTarget;
+    private GameObject targetDistance;
+    private LineRenderer lineRenderer;
+
+
     void Start()
     {
         initLeftFootPos = leftFootTarget.localPosition;
         lastLeftFootPos = leftFootTarget.position;
+
+        legGroundTarget = transform.Find("Model/BodyPivot/Body/LegTargetRaycast/LegGroundTarget").gameObject;
+        targetDistance = transform.Find("Model/BodyPivot/UpLegPivot/LegPivot/FootPivot/TargetDistance").gameObject;
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
     }
 
     
@@ -21,5 +31,13 @@ public class Spider : MonoBehaviour
     {
         leftFootTarget.position = lastLeftFootPos;
         lastLeftFootPos = leftFootTarget.position;
+
+        var positions = new Vector3[]
+        {
+            legGroundTarget.transform.position,
+            targetDistance.transform.position
+        };
+
+        lineRenderer.SetPositions(positions);
     }
 }
