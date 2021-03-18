d1cb41b1580134f137ec632f18cd4e2fdcf95599
biology-student <tsuka0625yskz@gmail.com>
2021-03-18 13:18:36 +0900

    Spider Step2 まで完成
    足を地面に固定し、Bodyを動かすとLegが動くようにした

---
+++ Spider/Assets/Spider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : MonoBehaviour
+{
+
+    public Transform leftFootTarget;
+
+    private Vector3 initLeftFootPos;
+
+    private Vector3 lastLeftFootPos;
+    
+    
+    void Start()
+    {
+        initLeftFootPos = leftFootTarget.localPosition;
+
+        lastLeftFootPos = leftFootTarget.position;
+    }
+
+    
+    void Update()
+    {
+        leftFootTarget.position = lastLeftFootPos;
+
+        lastLeftFootPos = leftFootTarget.position;
+    }
+}
